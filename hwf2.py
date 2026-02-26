2fe0872e301c0410f4e315f500da19e3c30c4910
AkmalDraga <suratmia21h@student.unhas.ac.id>
2026-02-27 07:05:17 +0800

    feb27

--- hwf2.py
+++ hwf2.py
@@ -33,17 +33,49 @@ wan_cent = wf_array.berry_phase(
         )
 wan_cent /= 2 * np.pi
 
+wan_cent_inter = wf_array.berry_phase(
+        axis_idx=1, state_idx=[1,2], contin=True, berry_evals=True
+        )
+
+fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+
+#intralayer case
 nky = wan_cent.shape[0]
 ky = np.linspace(0, 1, nky)
 
 flux = wf_array.berry_flux(plane=(0, 1), state_idx=[0,1], non_abelian=False)
 chern_kz = np.sum(flux, axis=(0,1)) / (2 * np.pi)
 print(chern_kz)
-#print(wan_cent)
 
 for shift in range(-2, 3):
-    plt.plot(ky, wan_cent[:, 0] + float(shift), "k")
-    plt.plot(ky, wan_cent[:, 1] + float(shift), "k")
+    ax[0].plot(ky, wan_cent[:, 0] + float(shift), "k")
+    ax[0].plot(ky, wan_cent[:, 1] + float(shift), "k")
+
+ax[0].set_ylabel("Wannier center along x")
+ax[0].set_xlabel(r"$k_y$")
+ax[0].set_xticks([0.0, 0.5, 1.0])
+ax[0].set_xlim(0.0, 1.0)
+ax[0].set_xticklabels([r"$0$", r"$\pi$", r"$2\pi$"])
+ax[0].axvline(x=0.5, linewidth=0.5, color="k")
+ax[0].set_title("1D Wannier centers: Intralayer Case")
+
+#interlayer case
+nky2 = wan_cent_inter.shape[0]
+ky2 = np.linspace(0, 1, nky2)
+
+for shift in range(-2, 3):
+    ax[1].plot(ky2, wan_cent_inter[:, 2] + float(shift), "k")
+    ax[1].plot(ky2, wan_cent_inter[:, 1] + float(shift), "k")
+
+ax[1].set_ylabel("Wannier center along z")
+ax[1].set_xlabel(r"$k_y$")
+ax[1].set_xticks([0.0, 0.5, 1.0])
+ax[1].set_xlim(0.0, 1.0)
+
+ax[1].set_xticklabels([r"$0$", r"$\pi$", r"$2\pi$"])
+ax[1].axvline(x=0.5, linewidth=0.5, color="k")
+
+ax[1].set_title("1D Wannier centers: Interlayer Case")
 
 plt.savefig("result/hwf.pdf")
 plt.show()
